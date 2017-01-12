f84b4ecd77c06f9b1f0fd41777325bbe6d0c553f
Alhassan Adullahi Haruna aka (Sudden) <sudden8317@gmail.com>
2017-01-12 16:24:31 +0100

    Update 0

--- Hey.cpp
+++ Hey.cpp
@@ -1,1 +1,15 @@
 //Hello Luqman!
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a, b, c;
+  c=a+b;
+  cout<<"Enter an integer a: ";
+  cin>>a;
+  cout<<"Enter an integer b: ";
+  cin>>b;
+  cout<<"The sum of c is: "<<c;
+  return 0;
+}
